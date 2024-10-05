c58efea85172e6da7ef88fb52b9425cfc8bd0d5c
Teemu Harju <harju9797@gmail.com>
2024-10-05 13:01:05 +0300

    Debugging layer added to vulkan.

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -13,6 +13,12 @@
 // static vulkan context
 static vulkan_context context;
 
+VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+		VkDebugUtilsMessageTypeFlagsEXT message_types,
+		const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+		void* user_data );
+
 b8 vulkan_renderer_backend_initialize( renderer_backend* backend, const char* application_name, struct platform_state* plat_state )
 {
 	// TODO: custom allocator.
@@ -97,10 +103,42 @@ b8 vulkan_renderer_backend_initialize( renderer_backend* backend, const char* ap
 
 	VK_CHECK( vkCreateInstance( &create_info, context.allocator, &context.instance ) );
 	KINFO( "Vulkan Instance created." );
+
+	// Debugger.
+#if defined( _DEBUG )
+	KDEBUG( "Creating Vulkan debugger..." );
+	u32 log_severity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT;  //|
+	//    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT;
+
+	VkDebugUtilsMessengerCreateInfoEXT debug_create_info = { VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT };
+	debug_create_info.messageSeverity = log_severity;
+	debug_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT;
+	debug_create_info.pfnUserCallback = vk_debug_callback;
+
+	PFN_vkCreateDebugUtilsMessengerEXT func = ( PFN_vkCreateDebugUtilsMessengerEXT )vkGetInstanceProcAddr( context.instance, "vkCreateDebugUtilsMessengerEXT" );
+	KASSERT_MSG( func, "Failed to create debug messenger!" );
+	VK_CHECK( func( context.instance, &debug_create_info, context.allocator, &context.debug_messenger ) );
+	KDEBUG( "Vulkan debugger created." );
+#endif
+
+	KINFO( "Vulkan renderer initialized successfully." );
 	return TRUE;
 }
+
 void vulkan_renderer_backend_shutdown( renderer_backend* backend )
 {
+	KDEBUG( "Destroying Vulkan debugger..." );
+	if( context.debug_messenger )
+	{
+		PFN_vkDestroyDebugUtilsMessengerEXT func =
+				( PFN_vkDestroyDebugUtilsMessengerEXT )vkGetInstanceProcAddr( context.instance, "vkDestroyDebugUtilsMessengerEXT" );
+		func( context.instance, context.debug_messenger, context.allocator );
+	}
+
+	KDEBUG( "Destroying Vulkan instance..." );
+	vkDestroyInstance( context.instance, context.allocator );
 }
 
 void vulkan_renderer_backend_on_resized( renderer_backend* backend, u16 width, u16 height )
@@ -114,4 +152,29 @@ b8 vulkan_renderer_backend_begin_frame( renderer_backend* backend, f32 delta_tim
 b8 vulkan_renderer_backend_end_frame( renderer_backend* backend, f32 delta_time )
 {
 	return TRUE;
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+		VkDebugUtilsMessageTypeFlagsEXT message_types,
+		const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+		void* user_data )
+{
+	switch( message_severity )
+	{
+	default:
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+		KERROR( callback_data->pMessage );
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+		KWARN( callback_data->pMessage );
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+		KINFO( callback_data->pMessage );
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+		KTRACE( callback_data->pMessage );
+		break;
+	}
+	return VK_FALSE;
 }
